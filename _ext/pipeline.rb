f20eaf167f49bb0df5ff9b7dbaf86384adbe604a
Ben Browning <bbrownin@redhat.com>
2010-09-16 19:55:23 -0400

    Initial awestruct template

---
+++ _ext/pipeline.rb
@@ -0,0 +1,9 @@
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Posts.new('/news')
+  extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5)
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
+  extension Awestruct::Extensions::Indexifier.new
+  # extension Awestruct::Extensions::IntenseDebate.new()
+end
+
