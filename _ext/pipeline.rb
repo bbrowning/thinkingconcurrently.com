99e66238c8273da11e29841a6cf2edc900a36c4c
Ben Browning <bbrownin@redhat.com>
2010-09-17 23:01:55 -0400

    4 posts per page and add Projects sidebar section

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,7 @@
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
-  extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 2)
+  extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 4)
   extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom')
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::IntenseDebate.new()
