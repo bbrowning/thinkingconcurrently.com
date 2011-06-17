83171ac7ed08b2a92d9abe5c844d3fc8524c08c8
Ben Browning <bbrownin@redhat.com>
2011-06-17 17:41:32 -0400

    Get order of extensions correct in the pipeline

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,8 +2,8 @@
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
   extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 4)
-  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom')
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom')
   extension Awestruct::Extensions::IntenseDebate.new()
 
   helper Awestruct::Extensions::GoogleAnalytics
